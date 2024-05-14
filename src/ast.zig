d82ef96e5c83f98dcf8d947141a77c4b228c1de0
SpandanBG <spandanbg@gmail.com>
2024-05-15 01:33:08 +0530

    Revert "add setup for expression parsing"
    This reverts commit a238c92f759a8230665599c347e14f4b8749de6c.

--- src/ast.zig
+++ src/ast.zig
@@ -23,7 +23,6 @@ pub fn destroy_node(node: *Node, allocator: Allocator) void {
 pub const Statement = union(enum) {
     LET: Let,
     RETURN: Return,
-    EXPR_STMT: ExpressionStmt,
 };
 
 pub const Expression = union(enum) {
@@ -39,5 +38,3 @@ pub const Let = struct { tkn: *Token, name: Identifier, value: Expression };
 pub const Identifier = struct { tkn: *Token, value: []const u8 };
 
 pub const Return = struct { tkn: *Token, return_value: Expression };
-
-pub const ExpressionStmt = struct { tkn: *Token, expr: Expression };
